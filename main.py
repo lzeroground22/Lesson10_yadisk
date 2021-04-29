39be63fa8622dd791620023fa7e5d283832641a7
Alex <User@Big-PC.les.local>
2021-04-29 17:40:23 +0300

    Working

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
 import requests
 
 
-def adr_parser(file_path):
-    """Функция получает имя файла из пути к нему"""
-    # adr = file_path
-    adr_split = file_path.replace("\\", " ")
-    adr_list = adr_split.split()
-    # filename = adr_list[-1]
-    return adr_list[-1]
-
-
 class YaUploader:
     def __init__(self, token: str):
         self.token = token
@@ -20,33 +11,33 @@ class YaUploader:
             'Authorization': 'OAuth {}'.format(self.token)
         }
 
+    def adr_parser(self, file_path):
+        """Функция получает имя файла из пути к нему"""
+        # adr = file_path
+        adr_split = file_path.replace("\\", " ")
+        adr_list = adr_split.split()
+        # filename = adr_list[-1]
+        return adr_list[-1]
+
     def get_upload_link(self, disk_file_path):
         """Получаем ссылку для загрузки файла на Я.Диск"""
         upload_url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
         headers = self.get_headers()
-        params = {'path': disk_file_path, 'owerwrite': 'true'}
+        params = {'path': disk_file_path, 'overwrite': 'true'}
         response = requests.get(upload_url, headers=headers, params=params)
         return response.json()
 
     def upload(self, file_path):
         """Метод загруджает файл file_path на Я.Диск"""
-        href = self.get_upload_link(disk_file_path=file_path).get('href', '')
-        filename = f"'{adr_parser(file_path)}'"
-        print(filename)
-        response = requests.put(href, data=open(filename, 'rb'))
+        filename = self.adr_parser(file_path)
+        href = self.get_upload_link(disk_file_path=filename).get('href', '')
+        response = requests.put(href, data=open(file_path, 'rb'))
         response.raise_for_status()
         if response.status_code == 201:
-            return "Success"
+            return f'File "{self.adr_parser(file_path)}" was created successfully'
 
 
 if __name__ == '__main__':
     uploader = YaUploader('AQAAAAA36m8ZAADLW6XIsrMVfk9ImIKjJD3zTy0')
-    result = uploader.upload("D:\Python\Lesson10_yadisk\Disk_file.txt")
-
-    # yandex_disk = YandexDisk(token=TOKEN)
-    # print(yandex_disk.get_upload_link('/Disk_file.txt'))
-    # print(yandex_disk.get_file_list())
-    # yandex_disk.upload_file_to_disk('Disk_file.txt', 'D:\Python\Disk_file.txt')
-
-# from ya_disk import YandexDisk
-# TOKEN = 'AQAAAAA36m8ZAADLW6XIsrMVfk9ImIKjJD3zTy0'
+    result = uploader.upload("D:\Python\Disk_file.txt")
+    print(result)
